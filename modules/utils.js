e7be03fd2e42b563188b1e4d4ff0c107c779a6d6
tushargandhi <gandhitushar702@gmail.com>
2024-02-03 17:19:17 -0800

    modifications

--- modules/utils.js
+++ modules/utils.js
@@ -1,5 +1,7 @@
 function getDate() {
-    const currentDate = new Date().toString();
+    const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    const currentDate = new Date().toLocaleString('en-US', options);
     return currentDate;
 }
-module.exports = {getDate};
+
+module.exports = { getDate };
