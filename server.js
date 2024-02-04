e95da0a4ab3c58e4fde83ba00ecb7554d28ce43f
tushargandhi <gandhitushar702@gmail.com>
2024-02-03 16:36:08 -0800

    changes in server.c and cleared contents in file.txt

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-
 const { getDate } = require('./modules/utils');
+const { greeting, serverMessage } = require('./lang/en/en');
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
 
- // Handling different routes
- if (parsedUrl.pathname === '/COMP4537/labs/3/getDate/') {
-    const name = parsedUrl.query.name || 'Guest';
-    //const greeting = require('./lang/en/en').greeting;
-    const { greeting, serverMessage } = require('./lang/en/en')
+    // Handling different routes
+    if (parsedUrl.pathname === '/COMP4537/labs/3/getDate/') {
+        const name = parsedUrl.query.name || 'Guest';
+        const currentDate = getDate();
+        const message = `<span style="color: blue;">${greeting} ${name}, ${serverMessage} ${currentDate}</span>`;
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(message);
+    } else if (parsedUrl.pathname === '/COMP4537/labs/3/writeFile/') {
+        const text = parsedUrl.query.text || '';
+        const fileName = 'file.txt';
 
-    const currentDate = getDate();
-    const message = `<span style="color: blue;">${greeting} ${name}, ${serverMessage} ${currentDate}</span>`;
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(message);
-} else if (parsedUrl.pathname === '/COMP4537/labs/3/writeFile/') {
-    const text = parsedUrl.query.text || '';
-    const fileName = 'file.txt';
+        fs.appendFileSync(fileName, `${text}\n`);
 
-    fs.appendFileSync(fileName, `${text}\n`);
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Text appended to file.');
-} else if (parsedUrl.pathname === '/COMP4537/labs/3/readFile/file.txt') {
-    const fileName = 'file.txt';
-
-    try {
-        const fileContent = fs.readFileSync(fileName, 'utf8');
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(fileContent);
-    } catch (error) {
+        res.end('Text appended to file.');
+    } else if (parsedUrl.pathname === '/COMP4537/labs/3/readFile/file.txt') {
+        const fileName = 'file.txt';
+
+        try {
+            const fileContent = fs.readFileSync(fileName, 'utf8');
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(fileContent);
+        } catch (error) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end(`File not found: ${fileName}`);
+        }
+    } else if (parsedUrl.pathname === '/COMP4537/labs/3/clearFile' || parsedUrl.pathname === '/COMP4537/labs/3/clearFile/') {
+        const fileName = parsedUrl.query.filename || 'file.txt';
+        
+        fs.writeFileSync(fileName, ''); 
+
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`File '${fileName}' contents cleared.`);
+    } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(`File not found: ${fileName}`);
+        res.end('Not Found');
     }
-} else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-}
 });
 
 const PORT = 3000;
@@ -48,5 +52,4 @@ server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-// Add this line to continuously log messages
 console.log('Server is listening...');
